c0d4f116705c5b8ba2c9db4647ec748cfb942c13
Jose Ambrosio <jjambrosio1@gmail.com>
2021-06-07 02:40:39 -0700

    setup room database. Need to add user

--- app/src/main/java/com/ambrosio/movietrove/database/MovieLocalModel.kt
+++ app/src/main/java/com/ambrosio/movietrove/database/MovieLocalModel.kt
@@ -11,51 +11,51 @@ import javax.inject.Inject
 
 const val TIMEOUT_DURATION_MILLIS = 3000L
 
-//class MovieLocalModel @Inject constructor() : IMovieModel {
+class MovieLocalModel @Inject constructor() : IMovieModel {
 
-//    private var databaseClient: RoomDatabaseClient =
-//        RoomDatabaseClient.getInstance(TroveApplication.instance.applicationContext)
-//
-//    private suspend fun performOperationWithTimeout(function: () -> Unit, callback: SuccessCallback){
-//        GlobalScope.launch {
-//            val job = async {
-//                try {
-//                    withTimeout(TIMEOUT_DURATION_MILLIS) {
-//                        function.invoke()
-//                    }
-//                    true
-//                } catch(e: java.lang.Exception) {
-//                    false
-//                }
-//            }
-//            callback.invoke(job.await())
-//        }
-//    }
-//
-//    override suspend fun addMovie(movie: Movie, callback: SuccessCallback){
-//        performOperationWithTimeout({databaseClient.movieDAO().addMovie(movie)}, callback)
-//    }
-//
-//    override suspend fun addMovies(movies: List<Movie>, callback: SuccessCallback){
-//        performOperationWithTimeout({databaseClient.movieDAO().insertAll(movies)}, callback)
-//    }
-//
-//    override suspend fun updateMovie(movie: Movie, callback: SuccessCallback){
-//        performOperationWithTimeout({
-//            databaseClient.movieDAO().updateMovie(movie)
-//        }, callback)
-//    }
-//
-//    override suspend fun deleteMovie(movie: Movie, callback: SuccessCallback){
-//        performOperationWithTimeout({databaseClient.movieDAO().deleteMovie(movie)}, callback)
-//    }
-//
-//    override suspend fun retrieveMovies(callback: (List<Movie>?) -> Unit){
-//        val job = GlobalScope.async {
-//            withTimeoutOrNull(TIMEOUT_DURATION_MILLIS){
-//                databaseClient.movieDAO().retrieveMovies()
-//            }
-//        }
-//        callback.invoke(job.await())
-//    }
-//}
+    private var databaseClient: RoomDatabaseClient =
+        RoomDatabaseClient.getInstance(TroveApplication.instance.applicationContext)
+
+    private suspend fun performOperationWithTimeout(function: () -> Unit, callback: SuccessCallback){
+        GlobalScope.launch {
+            val job = async {
+                try {
+                    withTimeout(TIMEOUT_DURATION_MILLIS) {
+                        function.invoke()
+                    }
+                    true
+                } catch(e: java.lang.Exception) {
+                    false
+                }
+            }
+            callback.invoke(job.await())
+        }
+    }
+
+    override suspend fun addMovie(movie: Movie, callback: SuccessCallback){
+        performOperationWithTimeout({databaseClient.movieDAO().addMovie(movie)}, callback)
+    }
+
+    override suspend fun addMovies(movies: List<Movie>, callback: SuccessCallback){
+        performOperationWithTimeout({databaseClient.movieDAO().insertAll(movies)}, callback)
+    }
+
+    override suspend fun updateMovie(movie: Movie, callback: SuccessCallback){
+        performOperationWithTimeout({
+            databaseClient.movieDAO().updateMovie(movie)
+        }, callback)
+    }
+
+    override suspend fun deleteMovie(movie: Movie, callback: SuccessCallback){
+        performOperationWithTimeout({databaseClient.movieDAO().deleteMovie(movie)}, callback)
+    }
+
+    override suspend fun retrieveMovies(callback: (List<Movie>?) -> Unit){
+        val job = GlobalScope.async {
+            withTimeoutOrNull(TIMEOUT_DURATION_MILLIS){
+                databaseClient.movieDAO().retrieveMovies()
+            }
+        }
+        callback.invoke(job.await())
+    }
+}
--- app/src/main/java/com/ambrosio/movietrove/network/APIConstants.kt
+++ app/src/main/java/com/ambrosio/movietrove/network/APIConstants.kt
@@ -3,4 +3,4 @@ package com.ambrosio.movietrove.network
 const val BASE_URL = "https://api.themoviedb.org/3/"
 const val BASE_POSTER_URL = "https://image.tmdb.org/t/p/w342"
 const val BASE_BACKDROP_URL = "https://image.tmdb.org/t/p/w780"
-const val API_KEY = "API_KEY"
+const val API_KEY = "b8c6cd83f369e56197205804c5d34f7b"
